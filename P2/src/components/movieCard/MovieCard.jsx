35a4cf84bc02c18e6fd179866b27395b304ea4f4
Samuel Argueta <arg211024@uvg.edu.gt>
2023-05-07 01:20:47 -0600

    Modificaciones finales del API, falta el registro de usuario

--- P2/src/components/movieCard/MovieCard.jsx
+++ P2/src/components/movieCard/MovieCard.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import './MovieCard.css'
+import styles from './MovieCard.module.css'
 
 export default function MovieCard ({movie}) {
   const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path
   return (
-    <li className="movieCard">
-      <Link to={"/movies/"+movie.id}>
+    <li className={styles.movieCard}>
+      <Link to={"/movies/"+movie.id}
+      className={styles.movieImage}>
         <img 
-          className="movieImage"
+          className={styles.movieImage}
           width={230}
           height={345}
           src={imageUrl}
           alt={movie.title} />
-        <div className="title">{movie.title}</div>
+        <div className={styles.title}>{movie.title}</div>
       </Link>
     </li>
   )
